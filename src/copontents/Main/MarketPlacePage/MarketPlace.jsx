06286b4e3a668c19557fe3407588de253adfa037
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-10-14 09:39:06 +0200

    Not_ready_MarketPlace_content

--- src/copontents/Main/MarketPlacePage/MarketPlace.jsx
+++ src/copontents/Main/MarketPlacePage/MarketPlace.jsx
@@ -1,17 +1,25 @@
 import { useState, useEffect } from "react"
 
+
 export default function MarketPlace() {
-    let tempCount = 25;
+    const [activ, setActiv] = useState('activ');
+
     return (
         <section className="MarketPlace">
             <header className="MarketPlace_header">
                 <h1>Browse Marketplace</h1>
-                <p>Browse through more than {tempCount}k NFTs on the NFT Marketplace.</p>
+                <p>Browse through more than 50k NFTs on the NFT Marketplace.</p>
                 <section className="MarktPlace_search">
                     <input type="text" placeholder="Search your favourite NFTs" />
                     <span className="material-symbols-outlined">search</span>
                 </section>
             </header>
+            <section className="MarketPlace_content">
+                <header className="MarketPlace_content_header">
+                    <button className="activ">NFTs <span>302</span> <span className="line"></span></button>
+                    <button>NFTs <span>302</span> <span className="line"></span></button>
+                </header>
+            </section>
         </section>
     )
 }
