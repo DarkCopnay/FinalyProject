ab933497e4968aa976956b3927873b425d514040
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-10-18 16:01:46 +0200

    add_not_ready_RanksPages

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,12 +3,13 @@ import PageTemplate from "./components/PageTemplate";
 import { urls } from "../copontents/Layout/Header/Header";
 
 export default function RoutesComponent( {Components} ) {
-    const {Root_Layout, HomePage, Page404, MarketPlace} = Components;
+    const {Root_Layout, HomePage, Page404, MarketPlace, RanksPage} = Components;
 
     return (
         <Routes>
             <Route index element={<PageTemplate Layout={Root_Layout} Content={HomePage} />} />
             <Route path={urls.market} element={<PageTemplate Layout={Root_Layout} Content={MarketPlace}/>} />
+            <Route path={urls.rank} element={<PageTemplate Layout={Root_Layout} Content={RanksPage}/>}/>
             <Route path="*" element={<Navigate to="/404"/>}/>
             <Route path="/404" element={<Page404 />}/>
         </Routes>
--- src/copontents/Components.jsx
+++ src/copontents/Components.jsx
@@ -1,5 +1,6 @@
 import HomePage from "./Main/HomePage/HomePage";
 import MarketPlace from "./Main/MarketPlacePage/MarketPlace";
+import RanksPage from "./Main/RanksPage/RanksPage";
 import Root_Layout from "./Layout/Root_Layout";
 import Page404 from './Layout/ErrorsPage/Page404'
 
@@ -7,5 +8,6 @@ export const Copnonets = {
     Root_Layout,  
     HomePage,
     MarketPlace,
+    RanksPage,
     Page404
 };
