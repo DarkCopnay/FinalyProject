70ec57ac392ba0fb33c06cbebcd34146575e4a81
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-10-21 16:18:00 +0200

    add_singUp
    And not ready validator input

--- src/copontents/Main/SingUpPage/SingUpPage.jsx
+++ src/copontents/Main/SingUpPage/SingUpPage.jsx
@@ -1,5 +1,21 @@
+import { assets } from "../../../assets/Assets";
+import InputValid from "./components/InputValid";
+
 export default function SingUpPage() {
+
     return (
-        <section className="SingUp"></section>
+        <section className="SingUp">
+            <img src={assets.SingUp.singup_img1} alt="singup_img_1"/>
+            <form className="SingUp_right">
+                <header>
+                    <h2>Create account</h2>
+                    <p>Welcome! enter your details and start creating, 
+                        collecting and selling NFTs.</p>
+                </header>
+                <section className="SingUp_right_content">
+                    <InputValid />
+                </section>
+            </form>
+        </section>
     )
 }
