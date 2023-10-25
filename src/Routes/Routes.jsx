c49009d5ac282d4b31845ccaee3c9f7e0a1e6541
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-10-26 00:44:19 +0200

    fix

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -10,7 +10,7 @@ export default function RoutesComponent( {Components} ) {
             <Route index element={<PageTemplate Layout={Root_Layout} Content={HomePage} />} />
             <Route path={urls.market} element={<PageTemplate Layout={Root_Layout} Content={MarketPlace}/>} />
             <Route path={urls.rank} element={<PageTemplate Layout={Root_Layout} Content={RanksPage}/>}/>
-            <Route path={urls.login} element={<PageTemplate Layout={Root_Layout} Content={Login} />}/>
+            {/* <Route path={urls.login} element={<PageTemplate Layout={Root_Layout} Content={Login} />}/> */}
             <Route path={urls.register} element={<PageTemplate Layout={Root_Layout} Content={Register}/>}/>
             <Route path="*" element={<Navigate to="/404"/>}/>
             <Route path="/404" element={<Page404 />}/>
