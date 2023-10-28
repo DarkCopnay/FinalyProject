14772c74a463f03aa1d4b15652e900b7d9b12d6d
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-10-28 09:53:58 +0200

    del ProfileSlice

--- src/copontents/Main/Profile/ProfilePage.jsx
+++ src/copontents/Main/Profile/ProfilePage.jsx
@@ -1,6 +1,5 @@
 import { assets } from "../../../assets/Assets";
 import { useState, useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import AxiosInit from "../../../axios/axiosInit";
 
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { NFTpostReducer } from "./sliceRedux/SlicePosts";
-import { ProfileReducer } from "./sliceRedux/SliceProfile";
 
 export const store = configureStore({
     reducer: {
         Market: NFTpostReducer,
-        Profiles: ProfileReducer
     }
 })
