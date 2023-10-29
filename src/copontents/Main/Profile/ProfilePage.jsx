effd6cf1910d7810f8b163702f01c3441736abc0
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-10-29 11:11:56 +0100

    Profile_update

--- src/copontents/Main/Profile/ProfilePage.jsx
+++ src/copontents/Main/Profile/ProfilePage.jsx
@@ -22,25 +22,51 @@ export default function ProfilePage() {
                 IsLoading ? <h2>Profile Loading...</h2>
                 :
                 <section className="Profile">
-                    <img className="Profile_Avatar" src={assets.upload.uplodaAvater} alt="Test"/>
+                    <section className="Profile_header_BG">
+                    </section>
                     <header className="Profile_header">
                         <section className="Profile_header_left">
-                            <h2>Animakid</h2>
-                            <section className="Profile_header_left_stat">
-                                    <ul className="Profile_header_left_stat_box">
-                                        <li>
-                                            <h3>250k+</h3>
-                                            <span>Volume</span>
-                                        </li>
-                                        <li>
-                                            <h3>250k+</h3>
-                                            <span>Volume</span>
-                                        </li>
-                                        <li>
-                                            <h3>250k+</h3>
-                                            <span>Volume</span>
-                                        </li>
-                                    </ul>
+                        <img className="Profile_Avatar" src={assets.upload.uplodaAvater} alt="Test"/>
+                            <h2>{data.nickname}</h2>
+                            <ul className="Profile_header_left_stat_box">
+                                <li>
+                                    <h3>0</h3>
+                                    <span>Volume</span>
+                                </li>
+                                <li>
+                                    <h3>0</h3>
+                                    <span>NFTs Create</span>
+                                </li>
+                                <li>
+                                    <h3>0</h3>
+                                    <span>Followers</span>
+                                </li>
+                            </ul>
+
+                            <section className="Profile_header_left_bio">
+                                <h3>Bio</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, dicta. 
+                                Ea cumque quia quidem minima sed aspernatur repellendus voluptatibus, modi 
+                                perferendis tenetur numquam nostrum provident, 
+                                iure culpa quisquam nemo necessitatibus.</p>
+                            </section>
+
+                            <section className="Profile_header_left_social">
+                                <h3>Links</h3>
+                                <section>
+                                    <a href="#" target="_blank">
+                                        <img src={assets.SocialLogo.Discord} alt="Discord"/>
+                                    </a>
+                                    <a href="#" target="_blank">
+                                    <img src={assets.SocialLogo.YouTube} alt="YouTube"/>
+                                    </a>
+                                    <a href="#" target="_blank">
+                                    <img src={assets.SocialLogo.Twitter} alt="Twitter"/>
+                                    </a>
+                                    <a href="#" target="_blank">
+                                    <img src={assets.SocialLogo.Instagram} alt="Instagram"/>
+                                    </a>
+                                </section>
                             </section>
                         </section>
                     </header>
