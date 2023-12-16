8575efc4b50ca8c19c17bc4e202f8d15a93f1b78
Maksim <56311335+remix2021687@users.noreply.github.com>
2023-12-16 21:45:31 +0100

    Lib FramerMotion

--- src/copontents/Main/Profile/ProfilePage.jsx
+++ src/copontents/Main/Profile/ProfilePage.jsx
@@ -1,8 +1,9 @@
-import { assets } from "../../../assets/Assets";
 import { useState, useEffect } from "react";
 import { useParams, NavLink, useNavigate } from "react-router-dom";
-import AxiosInit from "../../../axios/axiosInit";
+import { assets } from "../../../assets/Assets";
 import { jwtDecode } from "jwt-decode";
+import { motion } from "framer-motion";
+import AxiosInit from "../../../axios/axiosInit";
 import ProfilePageContent from "./components/ProfilePageContent";
 
 export default function ProfilePage() {
